911eed04562069436e352d88ec1ae681ed7367d8
Mounzer Majzoub <majzoub.m@proton.me>
2023-11-28 17:06:44 +0100

    a little lost trying to get un lost

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,4 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  has_many :booking
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get '/boats', to: 'boats#home', as: 'boats'
-  get '/boats/new', to: 'boats#new', as: 'new_boat'
-  post '/boats', to: 'boats#create'
-  get '/boats/:id', to: 'boats#show', as: 'boat'
-  get '/boats/:id/edit', to: 'boats#edit', as: 'edit_boat'
-  patch '/boats/:id', to: 'boats#update'
-  delete '/boats/:id', to: 'boats#destroy'
+  resources :users do
+    resources :boats
+  end
+  resources :bookings
   # Defines the root path route ("/")
   # root "posts#index"
 end
