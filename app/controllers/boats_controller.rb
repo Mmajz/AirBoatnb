67373fea102305d3bf9f4062e2d6d7d72743801c
ugorji eke <ugorjiekechibuzo@gmail.com>
2023-11-28 16:34:07 +0100

    Created the Route,Controller and View for the Boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,17 @@
 class BoatsController < ApplicationController
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.save
+  end
+
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:location, :price, :description, :title, :user_id)
+  end
 end
